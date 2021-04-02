45e391046a846629b56ec22c6d9da285f7eeb46f
Fabricio Manrique <fabricio.manrique@jamcity.com>
2021-04-02 16:05:53 -0300

    Created parallax background and stars particle system

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] private float BackgroundOffsetVelocity = 0.11f;
+
+    private Material cachedMaterial;
+
+    private void Awake()
+    {
+        cachedMaterial = this.GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        cachedMaterial.mainTextureOffset += new Vector2(0, BackgroundOffsetVelocity * Time.deltaTime);
+    }
+}
