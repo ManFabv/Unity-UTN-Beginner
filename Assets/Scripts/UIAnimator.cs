77c3ed243ac158a55462257edd16b2214b642a7b
Fabricio Manrique <fabricio.manrique@jamcity.com>
2021-04-02 21:18:35 -0300

    With minor fixes and added a loader screen

---
+++ Assets/Scripts/UIAnimator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class UIAnimator : MonoBehaviour
+{
+    private enum UIAnimatorState 
+    {
+        None,
+        FadeIn,
+        FadeOut
+    }
+
+    [SerializeField] private UIAnimatorState InitialState = UIAnimatorState.None;
+    
+    private void Awake()
+    {
+        string tweenToExecute = string.Empty;
+        if(InitialState != UIAnimatorState.None)
+        {
+            tweenToExecute = InitialState.ToString();
+        }
+        if(!string.IsNullOrEmpty(tweenToExecute))
+        {
+            Animator animator = this.GetComponent<Animator>();
+            animator.SetTrigger(tweenToExecute);
+        }
+    }
+}
