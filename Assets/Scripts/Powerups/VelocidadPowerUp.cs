94a11e381cb80802aef7213a8ef8d5a31863b955
Fabricio Manrique <fabricio.manrique@jamcity.com>
2021-04-09 12:56:55 -0300

    Added taken power up effect

--- Assets/Scripts/Powerups/VelocidadPowerUp.cs
+++ Assets/Scripts/Powerups/VelocidadPowerUp.cs
@@ -5,6 +5,7 @@ public class VelocidadPowerUp : MonoBehaviour
 #pragma warning disable 0649
     [SerializeField] private int Bonus = 2;
     [SerializeField] private float DuracionDeBonus = 5;
+    [SerializeField] private GameObject TakenEffect;
 #pragma warning restore 0649
 
     private void OnTriggerEnter(Collider other)
@@ -14,6 +15,12 @@ public class VelocidadPowerUp : MonoBehaviour
         if (movimientoPersonaje != null)
         {
             movimientoPersonaje.AplicarBonusVelocidad(Bonus, DuracionDeBonus);
+
+            if (TakenEffect != null)
+            {
+                Instantiate(TakenEffect, this.transform.position, this.transform.rotation);
+            }
+
             Destroy(this.gameObject);
         }
     }
---
+++ Assets/Scripts/Utilities/AutoDestroyParticleSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AutoDestroyParticleSystem : MonoBehaviour
+{
+    private ParticleSystem cachedParticleSystem;
+
+    private void Awake()
+    {
+        cachedParticleSystem = this.GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if(!cachedParticleSystem.IsAlive())
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
