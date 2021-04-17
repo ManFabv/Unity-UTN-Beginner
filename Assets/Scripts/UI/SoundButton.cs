a9511f9aaf861201e6dfd5009941a5be62d0fa2e
Fabricio Manrique <fabricio.manrique@jamcity.com>
2021-04-17 18:34:56 +0100

    Added sound to button clicks

---
+++ Assets/Scripts/UI/SoundButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Button))]
+public class SoundButton : MonoBehaviour
+{
+    private AudioSource cachedAudioSource;
+    private Button cachedButton;
+    
+    private void Awake()
+    {
+        cachedAudioSource = this.GetComponent<AudioSource>();
+        cachedAudioSource.loop = false;
+        cachedAudioSource.playOnAwake = false;
+        cachedAudioSource.Stop();
+        
+        cachedButton = this.GetComponent<Button>();
+        cachedButton.onClick.AddListener(PlaySound);
+    }
+
+    private void PlaySound()
+    {
+        cachedAudioSource.Play();
+    }
+}
