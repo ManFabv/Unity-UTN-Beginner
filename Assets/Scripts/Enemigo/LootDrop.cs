098ac95869bec935a5ce066f978019de6c011427
Fabricio Manrique <fabricio.manrique@jamcity.com>
2021-04-17 18:51:51 +0100

    I can have a random loot drop from enemies

---
+++ Assets/Scripts/Enemigo/LootDrop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class LootDrop : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] private GameObject[] LootPrefabs;
+    [SerializeField] private int LootProbability = 20;
+#pragma warning restore 0649
+
+    private void Awake()
+    {
+        if (LootPrefabs == null || LootPrefabs.Length == 0)
+            Debug.LogError("EL " + typeof(GameObject) + " ES NULO EN " + nameof(LootPrefabs));
+    }
+
+    public void Murio()
+    {
+        DropLoot();
+    }
+    
+    public void DropLoot()
+    {
+        if (LootPrefabs != null && LootPrefabs.Length > 0)
+        {
+            int randomProbability = Random.Range(0, 100);
+            bool canDropLoot = LootProbability <= randomProbability;
+
+            if (canDropLoot)
+            {
+                int lootPrefabIndex = Random.Range(0, LootPrefabs.Length);
+                GameObject loot = LootPrefabs[lootPrefabIndex];
+                Instantiate(loot, this.transform.position, this.transform.rotation);
+            }
+        }
+    }
+}
